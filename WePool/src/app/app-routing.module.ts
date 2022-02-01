52814262306e804cc5834da2f84087ca31fd3152
Ddoublej917 <djrobotics@ufl.edu>
2022-02-01 17:39:34 -0500

    Added homepage, starter components, routing, and toolbar

--- WePool/src/app/app-routing.module.ts
+++ WePool/src/app/app-routing.module.ts
@@ -1,8 +1,22 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AdministrationComponent } from './administration/administration.component';
+import { CarpoolsComponent } from './carpools/carpools.component';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { ModerationComponent } from './moderation/moderation.component';
+import { ProfileComponent } from './profile/profile.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'home', component: HomeComponent },
+  { path: 'carpools', component: CarpoolsComponent },
+  { path: 'profile', component: ProfileComponent },
+  { path: 'administration', component: AdministrationComponent },
+  { path: 'moderation', component: ModerationComponent },
+  { path: 'login', component: LoginComponent },
+  { path: '', redirectTo: '/home', pathMatch: 'full' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- WePool/src/app/app.module.ts
+++ WePool/src/app/app.module.ts
@@ -3,14 +3,38 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatCardModule } from '@angular/material/card';
+import { CarpoolsComponent } from './carpools/carpools.component';
+import { HomeComponent } from './home/home.component';
+import { ProfileComponent } from './profile/profile.component';
+import { AdministrationComponent } from './administration/administration.component';
+import { ModerationComponent } from './moderation/moderation.component';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CarpoolsComponent,
+    HomeComponent,
+    ProfileComponent,
+    AdministrationComponent,
+    ModerationComponent,
+    LoginComponent
   ],
   imports: [
+    MatToolbarModule,
+    MatButtonModule,
+    MatIconModule,
+    MatGridListModule,
+    MatCardModule,
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
