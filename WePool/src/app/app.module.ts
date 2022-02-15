53aa5b21b890d428e821e56dacb39202e042b468
Ddoublej917 <djrobotics@ufl.edu>
2022-02-15 17:26:31 -0500

    Implemented basic registration and login functionality

--- WePool/src/app/app.module.ts
+++ WePool/src/app/app.module.ts
@@ -6,6 +6,7 @@ import { AppComponent } from './app.component';
 
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatTabsModule } from '@angular/material/tabs';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { MatGridListModule } from '@angular/material/grid-list';
@@ -43,6 +44,7 @@ import { ToolbarComponent } from './toolbar/toolbar.component';
   imports: [
     MatToolbarModule,
     MatButtonModule,
+    MatTabsModule,
     MatIconModule,
     MatGridListModule,
     MatCardModule,
