1c9a49ed810f33562368d89f6bfb71c6a2fab3bb
Numbuh474 <35499003+Numbuh474@users.noreply.github.com>
2022-03-04 04:17:17 -0500

    Revert "Remove wrappers from returned JSON in Company and Location functions"
    This reverts commit a4816e341429a313270f70b1081b273b8199facd.

--- backend/src/controller/company.go
+++ backend/src/controller/company.go
@@ -16,7 +16,7 @@ func CreateCompany(c *gin.Context) {
 	// Validate input
 	var input CreateCompanyInput
 	if err := c.ShouldBindJSON(&input); err != nil {
-		c.JSON(http.StatusBadRequest, err.Error())
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
 		return
 	}
 
@@ -24,5 +24,5 @@ func CreateCompany(c *gin.Context) {
 	company := model.Company{Name: input.Name, Domain: input.Domain}
 	model.DB.Create(&company)
 
-	c.JSON(http.StatusOK, company)
+	c.JSON(http.StatusOK, gin.H{"data": company})
 }
