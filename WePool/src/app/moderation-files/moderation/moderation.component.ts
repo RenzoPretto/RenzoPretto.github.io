1fb4369979c5e21aa23a37713b76906e69442f40
Ddoublej917 <djrobotics@ufl.edu>
2022-04-01 10:50:51 -0400

    Setup moderation dashboard layout

--- WePool/src/app/moderation-files/moderation/moderation.component.ts
+++ WePool/src/app/moderation-files/moderation/moderation.component.ts
@@ -1,15 +1,33 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { map } from 'rxjs/operators';
+import { Breakpoints, BreakpointObserver } from '@angular/cdk/layout';
 
 @Component({
-  selector: 'app-moderation',
+  selector: 'app-mod',
   templateUrl: './moderation.component.html',
   styleUrls: ['./moderation.component.css']
 })
-export class ModerationComponent implements OnInit {
+export class ModerationComponent {
+  /** Based on the screen size, switch from standard to one column per row */
+  cards = this.breakpointObserver.observe(Breakpoints.Handset).pipe(
+    map(({ matches }) => {
+      if (matches) {
+        return [
+          { title: 'Carpool Groups', cols: 1, rows: 1 },
+          { title: 'User Join Requests', cols: 1, rows: 1 },
+          { title: 'Issues', cols: 1, rows: 1 },
+          { title: 'Company Join Requests', cols: 1, rows: 1 }
+        ];
+      }
 
-  constructor() { }
+      return [
+        { title: 'Carpool Groups', cols: 2, rows: 1 },
+        { title: 'User Join Requests', cols: 1, rows: 1 },
+        { title: 'Issues', cols: 1, rows: 2 },
+        { title: 'Company Join Requests', cols: 1, rows: 1 }
+      ];
+    })
+  );
 
-  ngOnInit(): void {
-  }
-
+  constructor(private breakpointObserver: BreakpointObserver) {}
 }
