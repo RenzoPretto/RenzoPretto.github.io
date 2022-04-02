8e84e49ebe253c75832e5f880f040023709c1fe0
Renzo Pretto <renzopretto@Renzos-MacBook-Pro.local>
2022-04-01 21:19:03 -0400

    Swapped pages.

--- WePool/src/app/moderation-files/moderation/moderation.component.ts
+++ WePool/src/app/moderation-files/moderation/moderation.component.ts
@@ -7,14 +7,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ModerationComponent implements OnInit {
 
-  userType : any;
-
   constructor() { }
 
   ngOnInit(): void {
-    //Initialize user type on initialization of page. Load in potential groups on user, "Please sign in!" on not signed in, 
-    //and "Group Selected!" on group already selected.
-    this.userType = "signedIn";
+    
   }
 
 }
