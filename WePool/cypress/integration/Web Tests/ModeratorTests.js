506046eb805d7cf0da5d72a08c922f87f0fcd1e8
Ddoublej917 <djrobotics@ufl.edu>
2022-04-02 18:38:37 -0400

    Created Cypress test for moderation page

---
+++ WePool/cypress/integration/Web Tests/ModeratorTests.js
@@ -0,0 +1,34 @@
+describe('Test group info component', () => {
+    it('Opens first rider\'s tab', () => {
+      cy.visit('http://localhost:4200/carpools')
+      cy.contains('Approve:').click()
+      cy.contains('Name:')
+      cy.wait(100)
+    })
+    it('Opens second rider\'s tab', () => {
+      cy.contains('Deny:').click()
+      cy.contains('Name:')
+      cy.wait(100)
+    })
+    it('Opens third rider\'s tab', () => {
+      cy.contains('Rider 3:').click()
+      cy.contains('Name:')
+      cy.wait(100)
+    })
+    it('Opens fourth rider\'s tab', () => {
+      cy.contains('Rider 4:').click()
+      cy.contains('Name:')
+      cy.wait(100)
+    })
+  })
+  
+  describe('Test directions scrolling.', () => {
+    it('Scrolls halfway through the directions.', () => {
+      cy.get('span#sidebar').scrollTo('center')
+      cy.wait(100)
+    })
+    it('Scrolls to the end of the directions.', () => {
+      cy.get('span#sidebar').scrollTo('bottom')
+      cy.wait(100)
+    })
+  })
