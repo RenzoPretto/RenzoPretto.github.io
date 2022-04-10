594bc55c6d7e254322878c629b4ae8d855f83d7b
Renzo Pretto <renzopretto@Renzos-MacBook-Pro.local>
2022-04-09 23:24:58 -0400

    Groups get organized by matching.

--- WePool/src/app/services/user-service/user.service.ts
+++ WePool/src/app/services/user-service/user.service.ts
@@ -14,6 +14,16 @@ export class UserService {
   //Pass in user, build url to get profile info
   async getUser(user : string) {
     let url = user;
+    /*return this.http.post("http://localhost:8000/employee/profile",
+    {
+      "workEmail": user
+    }).toPromise()
+    .then(
+      res => { // Success
+        console.log(res);
+        return parseJSON(res);
+      }
+    );*/
     return this.http.get(this._userUrl).toPromise()
     .then(
       res => { // Success
@@ -23,26 +33,22 @@ export class UserService {
   } 
 
   //Compares preferences between user and group and returns the amount of matches
-  async comparePrefs(groupID: any): Promise<number>{
+  async comparePrefs(group: any): Promise<number>{
     let userData = await this.getUser("renzo");
     userData = userData.Preferences;
-    console.log(userData);
-    let groupData = await this.groupService.getGroupPreferences(groupID);
-    console.log(groupData);
+    let groupData = group.Preferences;
     let count = 3;
-    if (Math.abs(parseInt(userData[6])-parseInt(groupData[6])) <= 1) {
+    if (Math.abs(userData[5]-groupData[5]) <= 1) {
       count++;
     }
-    if (Math.abs(parseInt(userData[7])-parseInt(groupData[7])) <= 1) {
+    if (Math.abs(userData[6]-groupData[6]) <= 1) {
+      count++;
+    }    if (Math.abs(userData[7]-groupData[7]) <= 1) {
       count++;
     }
-    if (Math.abs(parseInt(userData[8])-parseInt(groupData[8])) <= 1) {
-      count++;
+    if (userData[9] != groupData[9] || userData[10] != groupData[10] || userData[11] != groupData[11]) {
+      count = 0;
     }
-    if (userData[10] != groupData[10] || userData[11] != groupData[11] || userData[12] != groupData[12]) {
-      return 0;
-    }
-    console.log(count);
     return count;
   }
 
@@ -50,7 +56,6 @@ export class UserService {
   parsePhoneNumber(phoneNum: string): string {
     let formattedNum = "";
     formattedNum += "(" + phoneNum.substring(0,3) + ")" + phoneNum.substring(3,6) + "-" + phoneNum.substring(6,10);
-    console.log(formattedNum);
     return formattedNum;
   }
 }
